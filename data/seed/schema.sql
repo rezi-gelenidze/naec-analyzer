c3f60b74e820a0dcaf93bc7ef034308e8af97da4
rezi-gelenidze <rezi.gelenidze7@gmail.com>
2025-01-22 17:03:40 +0400

    add rows in exam for maintaining min and max score of specific year exam

--- data/seed/schema.sql
+++ data/seed/schema.sql
@@ -29,9 +29,9 @@ CREATE TABLE IF NOT EXISTS university
 
 CREATE TABLE IF NOT EXISTS faculty
 (
-    id            CHAR(11) PRIMARY KEY,                             -- The ID of the faculty is an 8-character string.
-    name          VARCHAR(511) NOT NULL,                           -- The name of the faculty.
-    university_id CHAR(3)      NOT NULL,                           -- FK to the university table.
+    id            CHAR(11) PRIMARY KEY,  -- The ID of the faculty is an 8-character string.
+    name          VARCHAR(511) NOT NULL, -- The name of the faculty.
+    university_id CHAR(3)      NOT NULL, -- FK to the university table.
     FOREIGN KEY (university_id) REFERENCES university (id)
 );
 
@@ -40,18 +40,20 @@ CREATE TABLE IF NOT EXISTS enrollment
 (
     student_id    CHAR(9) PRIMARY KEY, -- Each student can only enroll once. Same person on different years will have different student IDs.
     faculty_id    CHAR(11) NOT NULL,   -- FK to the faculty table.
-    contest_score FLOAT   NOT NULL,
-    rank          INTEGER NOT NULL,
-    year          INTEGER NOT NULL,
+    contest_score FLOAT    NOT NULL,
+    rank          INTEGER  NOT NULL,
+    year          INTEGER  NOT NULL,
     FOREIGN KEY (faculty_id) REFERENCES faculty (id)
 );
 
 -- Record for enrollment results for specific subjects.
 CREATE TABLE IF NOT EXISTS result
 (
-    enrollment_id CHAR(9) NOT NULL,  -- FK to enrollment table.
+    enrollment_id CHAR(9)      NOT NULL, -- FK to enrollment table.
     subject_name  VARCHAR(255) NOT NULL, -- FK to subject table.
-    scaled_score  FLOAT   NOT NULL,
+    scaled_score  FLOAT        NOT NULL, -- Scaled score of the student.
+    raw_score     FLOAT,                 -- Raw score of the student (before scaling).
+
     PRIMARY KEY (enrollment_id, subject_name),
     FOREIGN KEY (enrollment_id) REFERENCES enrollment (student_id),
     FOREIGN KEY (subject_name) REFERENCES subject (name)
@@ -60,11 +62,47 @@ CREATE TABLE IF NOT EXISTS result
 -- Record for acquired grants.
 CREATE TABLE IF NOT EXISTS grant
 (
-    student_id   CHAR(9) PRIMARY KEY, -- FK to enrollment table.
-    grant_score  FLOAT       NOT NULL, -- Cumulative score of the student.
-    grant_amount INTEGER     NOT NULL, -- 50%, 75%, or 100%.
+    student_id   CHAR(9) PRIMARY KEY,   -- FK to enrollment table.
+    grant_score  FLOAT        NOT NULL, -- Cumulative score of the student.
+    grant_amount INTEGER      NOT NULL, -- 50%, 75%, or 100%.
     subject_name VARCHAR(255) NOT NULL, -- FK to subject table.
     year         INTEGER      NOT NULL,
     FOREIGN KEY (student_id) REFERENCES enrollment (student_id),
     FOREIGN KEY (subject_name) REFERENCES subject (name)
 );
+
+
+-- Record for specific year subject exam statistical properties.
+CREATE TABLE IF NOT EXISTS exam
+(
+    subject_name       VARCHAR(255), -- FK to subject table.
+    year               INTEGER NOT NULL,         -- The year of the exam.
+
+    -- Point range
+    max_score          INTEGER,                   -- Maximum score of the subject.
+    min_score          INTEGER,                   -- Minimum score of the subject (barrier).
+
+    -- Statistical Properties
+    mean               FLOAT,         -- Mean score of the subject.
+    standard_deviation FLOAT,          -- Standard deviation of the subject.
+
+    PRIMARY KEY (subject_name, year) -- Composite primary key.
+);
+
+
+-- Seed last 4 year exam data for each subject (mean and standard deviation will be calculated later)
+INSERT INTO exam (subject_name, year, max_score, min_score) VALUES
+('MATHEMATICS', 2021, 51, 11),
+('MATHEMATICS', 2022, 51, 11),
+('MATHEMATICS', 2023, 51, 11),
+('MATHEMATICS', 2024, 51, 11),
+
+('FOREIGN LANGUAGE', 2021, 80, 16),
+('FOREIGN LANGUAGE', 2022, 80, 16),
+('FOREIGN LANGUAGE', 2023, 70, 14),
+('FOREIGN LANGUAGE', 2024, 70, 14),
+
+('GEORGIAN LANGUAGE', 2021, 70, 17),
+('GEORGIAN LANGUAGE', 2022, 60, 15),
+('GEORGIAN LANGUAGE', 2023, 60, 15),
+('GEORGIAN LANGUAGE', 2024, 60, 15);
